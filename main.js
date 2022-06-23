cf9242c0271b02a02ed83a96169597f5d6a371fb
Ashish K <ashishskabade@gmail.com>
2022-06-23 18:36:35 +0530

    add to cart

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ window.addEventListener('DOMContentLoaded',()=>{
     .then((res)=>{
         products=res.data;
         showProducts(products);
+        axios.get('http://localhost:3000/cart')
+        .then(res1=>{
+            for(x of res1.data){
+                showCartItems(x)
+            }
+        });
     })
 })
 const cartPop=document.getElementById('cartPop');
@@ -15,37 +21,50 @@ const total=document.getElementById('total');
 var cartObj=[];
 var cartItems=[];
 function addtocart(){
+    cart.innerHTML='<div id="cart-container"><nav class="cartTable" id="cartTable"><span class="cart-item">Item</span><span class="cart-price">Price</span><span class="cart-qty">Quantity</span></nav></div>';
     const id=(event.target.parentNode.id);
+    console.log(id)
+    const params = new URLSearchParams();
+    params.append('id', id);
+    axios.post('http://localhost:3000/cart', params)
+    .then((res)=>{
+        createNotif(name)
+        axios.get('http://localhost:3000/cart')
+        .then(res1=>{
+            for(x of res1.data){
+                showCartItems(x)
+            }
+        });
+    })
+        .catch((err)=>crossOriginIsolated.log(err));
     // console.log(event.path[1].children[0],event.path[1].children[1],event.path[1].children[2].value);
     // console.log(event.path[1].children[1])
-    const name=document.querySelector(`#${id} h2`).innerText;
-    const img=document.querySelector(`#${id} img`).src;
-    const price=document.querySelector(`#${id} p`).innerText;
-    let cartTotal=0;
-    obj={
-        name,img,price
-    };
-    for(x of cartObj){
-        if(x.name===name){
-            alert('Please increase the quantity in cart');
-            return;
-        }
-    }
-    cartObj.push(obj);
-    cartItems.push(obj)
-    console.log(cartObj,cartItems);
-    cart.innerHTML='<div id="cart-container"><nav class="cartTable" id="cartTable"><span class="cart-item">Item</span><span class="cart-price">Price</span><span class="cart-qty">Quantity</span></nav></div>';
-    for(x of cartObj){
-        showCartItems(x)
-        cartTotal+=parseInt(x.price);
-    }
-    createNotif(name)
-    total.innerText=`Total: $ ${cartTotal}`;
+    // const name=document.querySelector(`#${id} h2`).innerText;
+    // const img=document.querySelector(`#${id} img`).src;
+    // const price=document.querySelector(`#${id} p`).innerText;
+    // let cartTotal=0;
+    // obj={
+    //     name,img,price
+    // };
+    // for(x of cartObj){
+    //     if(x.name===name){
+    //         alert('Please increase the quantity in cart');
+    //         return;
+    //     }
+    // }
+    // cartObj.push(obj);
+    // cartItems.push(obj)
+    // console.log(cartObj,cartItems);
+    
+    
     
 }
 function showCartItems(obj){
+    console.log(obj.productData);
     const nav=document.createElement('nav');
-    nav.innerHTML=`<span class="cart-item"><img src="${obj.img}" >${obj.name}</span><span class="cart-price">${obj.price}</span><span class="cart-qty"><input type="number" value="1"><button onclick="remove()" >remove</button></span>`;
+    nav.innerHTML=`<span class="cart-item"><img src="${obj.productData.imageUrl}" >${obj.productData.title}</span>
+    <span class="cart-price">${obj.productData.price}</span><span class="cart-qty">
+    <input type="number" value="${obj.qty}"><button onclick="remove()" >remove</button></span>`;
     nav.className='cartTable';
     table.appendChild(nav);
 }
